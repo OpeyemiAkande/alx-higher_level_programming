91a7c252ef6d4d4064075b1d042df414724c4db0
Opeyemi Akande <akande212@gmail.com>
2022-06-03 11:00:13 +0100

    made a mistake on the first commit

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """A function that prints integers
+
+    Args:
+        my_list: list of integers to be printed
+
+    Returns: None
+
+    """
+    for element in my_list:
+        print("{}".format(element))
