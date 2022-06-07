f2eb2ad84811abdbc91389224b67d30f014894d4
Opeyemi Akande <akande212@gmail.com>
2022-06-07 14:42:22 +0100

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,8 +9,9 @@ def print_matrix_integer(matrix=[[]]):
 
     """
     for row in matrix:
-        for i in range(len(row)):
-            if i == len(row) - 1:
-                print("{:d}".format(row[i]), end="\n")
+        for i in row:
+            if i != row[-1]:
+                print("{:d}".format(i), end=" ")
             else:
-                print("{:d}".format(row[i]), end=" ")
+                print("{:d}".format(i), end="")
+        print()
